f93da3d3b2a21c1a9c6e9fd24eb4a62f4b692050
Vaqif <taqiyevv937@gmail.com>
2023-02-13 12:14:01 +0400

    [MASTER] toast message succesful creeted

--- app/src/main/java/com/vagif_tagiyev/batterylevelcontrolappwithbroadcastreceiver/BatteryLevelDetector.kt
+++ app/src/main/java/com/vagif_tagiyev/batterylevelcontrolappwithbroadcastreceiver/BatteryLevelDetector.kt
@@ -3,6 +3,7 @@ package com.vagif_tagiyev.batterylevelcontrolappwithbroadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class BatteryLevelDetector : BroadcastReceiver() {
 
@@ -12,6 +13,7 @@ class BatteryLevelDetector : BroadcastReceiver() {
 
     override fun onReceive(p0: Context?, p1: Intent?) {
 
+        Toast.makeText(p0, "Battery is low", Toast.LENGTH_SHORT).show()
 
     }
 }
