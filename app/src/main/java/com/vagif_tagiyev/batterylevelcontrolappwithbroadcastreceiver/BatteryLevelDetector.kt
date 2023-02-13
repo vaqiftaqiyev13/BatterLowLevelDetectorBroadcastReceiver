6ba39a109063596dae8ee82abc4f0df09771c74f
Vaqif <taqiyevv937@gmail.com>
2023-02-13 12:08:45 +0400

    [MASTER] BatteryLevelDetector class was created and Broadcast Receiver class is implmeneted

---
+++ app/src/main/java/com/vagif_tagiyev/batterylevelcontrolappwithbroadcastreceiver/BatteryLevelDetector.kt
@@ -0,0 +1,17 @@
+package com.vagif_tagiyev.batterylevelcontrolappwithbroadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BatteryLevelDetector : BroadcastReceiver() {
+
+    /**
+     * I create my onReceive method.
+     */
+
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+
+    }
+}
