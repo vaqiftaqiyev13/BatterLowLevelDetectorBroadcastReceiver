842cea43a50a0bdeae65dc5975aaa4d1a109332a
Vaqif <taqiyevv937@gmail.com>
2023-02-13 12:22:01 +0400

    [MASTER] object from batteryleveldetector class was declared

--- app/src/main/java/com/vagif_tagiyev/batterylevelcontrolappwithbroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/vagif_tagiyev/batterylevelcontrolappwithbroadcastreceiver/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.vagif_tagiyev.batterylevelcontrolappwithbroadcastreceiver
 
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.logging.Filter
 
 class MainActivity : AppCompatActivity() {
+    //battery level detector object was created because in new
+    // android broadcast list don't mention about battery low.
+    private lateinit var batteryLevelDetector: BatteryLevelDetector
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        val filter = IntentFilter("android.intent.action.BATTERY_LOW")
+        super.onResume()
+        registerReceiver(batteryLevelDetector, filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(batteryLevelDetector)
+    }
+
 }
